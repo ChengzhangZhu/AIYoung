59337a65363f6a90c4e52224b580f0675ae11633
Chengzhang Zhu <chengzhang.zhu@aiyoung.com.cn>
2018-07-12 11:38:08 +1000

    build the data model

--- team/admin.py
+++ team/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, Publication, Experience
 
 # Register your models here.
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('staff_id', 'en_name','cn_name', 'position')
-    list_filter = ('department','position',)
-    search_fields = ('cn_name', 'staff_id')
+    list_display = ('staff_id', 'name_en','title','name')
+    list_filter = ('title',)
+    search_fields = ('name', 'staff_id')
 #    prepopulated_fields = {'slug': ('title',)}
 #    raw_id_fields = ('author',)
 #    date_hierarchy = 'publish'
-    ordering = ['staff_id', 'en_name']
+    ordering = ['staff_id', 'name_en']
 
     
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Publication)
+admin.site.register(Experience)
--- team/models.py
+++ team/models.py
@@ -4,14 +4,57 @@ from django.urls import reverse
 
 class Member(models.Model):
     staff_id = models.CharField(max_length = 20, primary_key = True)
-    en_name = models.CharField(max_length = 250)
-    cn_name = models.CharField(max_length = 250)
+    name_en = models.CharField(max_length = 250)
+    name = models.CharField(max_length = 250)
+    dob = models.DateField()
+    title_choice = (('Mr', 'Mr.'),('Mrs','Mrs.'),('Miss','Miss'),('Dr','Dr.'),('Prof','Prof.'),('A_Prof','A/Prof.'))
+    title = models.CharField(max_length = 10, choices = title_choice, default = 'Mr')
+    email = models.EmailField()
+    telephone = models.CharField(max_length = 20)
+    biograph = models.TextField()
+    research_interests = models.TextField()
+    photo = models.ImageField(upload_to='img/')
     abstract = models.TextField()
     position = models.CharField(max_length = 250)
     img = models.ImageField(upload_to='member/')
-    department = models.CharField(max_length = 250)
+    
     def get_absolute_url(self):
         return reverse('member:detail',
                        args=[self.staff_id])
     def __str__(self):
-        return self.cn_name
+        return self.name
+    
+class Publication(models.Model):
+    publication_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 500)
+    abstract = models.TextField()
+    year = models.IntegerField()
+    venue_name = models.CharField(max_length = 500)
+    publication_choice = (('conf','conference'),('jour','journal'),('patn','patent'))
+    publication_type = models.CharField(max_length= 4, choices = publication_choice)
+    authors = models.TextField()
+    volume = models.CharField(max_length = 20)
+    number_v = models.CharField(max_length = 20)
+    page_v = models.CharField(max_length = 20)
+    source_code = models.FileField(upload_to='codes/')
+    manuscript = models.FileField(upload_to='manuscript/')
+    demo_link = models.SlugField()
+    staff_id = models.ForeignKey(to=Member, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+class Experience(models.Model):
+    experience_id = models.AutoField(primary_key=True)
+    start_time = models.DateField()
+    end_time = models.DateField()
+    experience_name = models.CharField(max_length = 500)
+    description = models.TextField()
+    main_task = models.TextField()
+    staff_id = models.ForeignKey(to = Member, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.experience_name
+        
+    
+    
