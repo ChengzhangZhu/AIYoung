5c04f2a9405298aa1f72aaab393503ed4caf55b3
Chengzhang Zhu <chengzhang.zhu@aiyoung.com.cn>
2018-07-15 09:24:50 +1000

    revised the required fields

--- team/models.py
+++ team/models.py
@@ -5,18 +5,18 @@ from django.urls import reverse
 class Publication(models.Model):
     publication_id = models.AutoField(primary_key = True)
     title = models.CharField(max_length = 500)
-    abstract = models.TextField()
+    abstract = models.TextField(blank=True)
     year = models.IntegerField()
     venue_name = models.CharField(max_length = 500)
     publication_choice = (('conf','conference'),('jour','journal'),('patn','patent'))
     publication_type = models.CharField(max_length= 4, choices = publication_choice)
     authors = models.TextField()
-    volume = models.CharField(max_length = 20)
-    number_v = models.CharField(max_length = 20)
-    page_v = models.CharField(max_length = 20)
-    source_code = models.FileField(upload_to='codes/')
-    manuscript = models.FileField(upload_to='manuscript/')
-    demo_link = models.SlugField()
+    volume = models.CharField(max_length = 20,blank=True)
+    number_v = models.CharField(max_length = 20,blank=True)
+    page_v = models.CharField(max_length = 20,blank=True)
+    source_code = models.FileField(upload_to='codes/', blank=True)
+    manuscript = models.FileField(upload_to='manuscript/', blank=True)
+    demo_link = models.CharField(max_length = 200, blank=True)
     #staff_id = models.ForeignKey(to=Member, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -44,15 +44,15 @@ class Member(models.Model):
     title_choice = (('Mr', 'Mr.'),('Mrs','Mrs.'),('Miss','Miss'),('Dr','Dr.'),('Prof','Prof.'),('A_Prof','A/Prof.'))
     title = models.CharField(max_length = 10, choices = title_choice, default = 'Mr')
     email = models.EmailField()
-    telephone = models.CharField(max_length = 20)
-    biograph = models.TextField()
+    telephone = models.CharField(max_length = 20, blank=True)
+    biograph = models.TextField(blank=True)
     research_interests = models.TextField()
     photo = models.ImageField(upload_to='member/img/')
-    abstract = models.TextField()
-    position = models.CharField(max_length = 250)
+    abstract = models.TextField(blank=True)
+    position = models.CharField(max_length = 250, blank=True)
     # edited by yizheng
-    publication_list = models.ManyToManyField(Publication, help_text="Select the publication for this member")
-    experience_list = models.ManyToManyField(Experience, help_text="Select the experience for this member")
+    publication_list = models.ManyToManyField(Publication, help_text="Select the publication for this member",blank=True)
+    experience_list = models.ManyToManyField(Experience, help_text="Select the experience for this member",blank=True)
     # edited by yizheng
 
     def get_absolute_url(self):
